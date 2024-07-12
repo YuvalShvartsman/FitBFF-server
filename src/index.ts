67390dec4c5e32d99766d743ef59ea970f0eb065
Yuval Shvartsman <yuval.shvartsman@gmail.com>
2024-07-12 11:42:24 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+import dotenv from "dotenv";
+
+// import routes from "./routes";
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your React app's URL
+    credentials: true,
+  })
+);
+
+// app.use(routes);
+
+try {
+  mongoose.connect("mongodb://localhost:27017/LazyDiet").then(() => {
+    console.log("established connection to mongodb");
+  });
+} catch (error) {
+  console.log("could not establish connection to mongodb", error);
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
