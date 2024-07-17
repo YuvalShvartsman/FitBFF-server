497f7a434a97e8ae6efbce067b60848c342bfa5c
benada1 <benadziashvili@gmail.com>
2024-07-17 18:43:44 +0300

    modified usersController so that it can distinguish whether the user exists or not, if not it creates a user in the database and if it does it updates the user details in case they got changed, and sends back a success response

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
+import { OAuth2Client } from "google-auth-library";
 
 import Users from "../models/Users";
 
+const client = new OAuth2Client(
+  "20513430831-2s88uppgtrbfomn25p7ooui5qfmluv7k.apps.googleusercontent.com"
+);
 interface GoogleSignInRequest extends Request {
   body: {
     token: string;
@@ -10,6 +14,31 @@ interface GoogleSignInRequest extends Request {
 
 export const googleSignIn = async (req: GoogleSignInRequest, res: Response) => {
   const { token } = req.body;
-  const users = Users.find();
-  console.log(token);
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience:
+        "20513430831-2s88uppgtrbfomn25p7ooui5qfmluv7k.apps.googleusercontent.com",
+    });
+    const payload = ticket.getPayload();
+    const { sub: googleId, email, name, picture } = payload;
+    const bla = await Users.findOne({ googleId: googleId });
+    console.log(bla);
+    console.log(googleId);
+
+    if (!bla) {
+      const user = await Users.create({ googleId, email, name, picture });
+      /* console.log("🚀 ~ googleSignIn ~ user:", user);*/
+    } else {
+      const user = await Users.updateOne(
+        { googleId },
+        { googleId, email, name, picture }
+      );
+    }
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    /* console.log("🚀 ~ googleSignIn ~ error:", error);*/
+    res.status(401).json({ error: "Invalid token" });
+  }
 };
